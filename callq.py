3429c6e5c06df14d3a6886dc9c2d1a1a5e1ea704
HiDE <vinasaigon@gmail.com>
2013-05-15 01:20:57 +0900

    divide call proc from main.py

--- callq.py
+++ callq.py
@@ -3,16 +3,18 @@
 import time
 import sqlite3
 from dbmanage import sqlitedb
+from twiliomanage import twiliomanage
 
 class callQ(object):
-
     queue = "baristaCall"
-
     @staticmethod
     def perform(eventid):
-
-        print (eventid)
-        #db = sqlitedb()
-        #eventrec = db.getactiveevent()
-        #for event in eventrec:
-        #    print event
+        print 'callQ eventid=%s' % eventid
+        db = sqlitedb()
+        dbinfos = db.getactivecall(eventid)
+        print 'dbinfos %s' % dbinfos
+        tw = twiliomanage()
+        for dbinfo in dbinfos:
+            print dbinfo[0]
+            #result = tw.call(dbinfo[0], dbinfo[1], telno, testflg, hostname, message, headid, footid)
+            result = tw.call('ichiro', 'suzuki', '0000', '1', 'hoge.example.com', 'message is hoge', '1', '1')
