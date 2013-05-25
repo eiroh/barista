10d8f851b7d2182e80cba297152b788239c0dc58
HiDE <vinasaigon@gmail.com>
2013-05-26 00:27:23 +0900

    add proc of call main

--- callq.py
+++ callq.py
@@ -10,32 +10,32 @@ class callQ(object):
     def perform(eventid):
         print 'callQ eventid=%s' % eventid
         db = sqlitedb()
-        #evinfo = db.geteventinfo(eventid)
-        #evinfo_status = evinfo[1] 
-        #evinfo_testflg = evinfo[2]
-        #evinfo_hostname = evinfo[3]
-        #evinfo_operator = evinfo[4]
-        #evinfo_calltype = evinfo[5]
-        #evinfo_frequency = evinfo[6]
-        #evinfo_language = evinfo[7]
-        #evinfo_message = evinfo[8]
-        #evinfo_headid = evinfo[9]
-        #evinfo_footid = evinfo[10]
-        #evinfo_lastnum = evinfo[11]
+        evinfo = db.geteventinfo(eventid)
+        info = evinfo[0]
+        evinfo_status = info[1] 
+        evinfo_testflg = info[2]
+        evinfo_hostname = info[3].encode('utf-8')
+        evinfo_operator = info[4].encode('utf-8')
+        evinfo_calltype = info[5]
+        evinfo_frequency = info[6]
+        evinfo_language = info[7].encode('utf-8')
+        evinfo_message = info[8].encode('utf-8')
+        evinfo_headid = info[9]
+        evinfo_footid = info[10]
+        evinfo_lastnum = info[11]
         dbinfos = db.getactivecall(eventid)
-        print 'dbinfos %s' % dbinfos
+        #print 'dbinfos %s' % dbinfos
         tw = twiliomanage()
         for dbinfo in dbinfos:
-            #dbinfo_eventid = dbinfo[0]
-            #dbinfo_numorder = dbinfo[1]
-            #dbinfo_ghid = dbinfo[2]
-            #dbinfo_name = dbinfo[3]
-            #dbinfo_telno = dbinfo[4]
-            #dbinfo_sleep = dbinfo[5]
-            #dbinfo_callid = dbinfo[6]
-            #dbinfo_attempt = dbinfo[7]
-            #dbinfo_latesttime = dbinfo[8]
-            #dbinfo_lateststatus = dbinfo[9]
-            #print 'eventid=%s, numorder=%s, ghid=%s, name=%s, telno=%s, sleep=%s, callid=%s, attempt=%s, latesttime=%s, lateststatus=%s' % (dbinfo[0],dbinfo[1],dbinfo[2],dbinfo[3],dbinfo[4],dbinfo[5],dbinfo[6],dbinfo[7],dbinfo[8],dbinfo[9])
-            #result = tw.call(dbinfo_ghid, dbinfo_name, dbinfo_telno, evinfo_testflg, evinfo_hostname, evinfo_message, evinfo_headid, evinfo_footid) 
-            result = tw.call('ichiro', 'suzuki', '0000', '1', 'hoge.example.com', 'message is hoge', '1', '1')
+            dbinfo_eventid = dbinfo[0]
+            dbinfo_numorder = dbinfo[1]
+            dbinfo_ghid = dbinfo[2].encode('utf-8')
+            dbinfo_name = dbinfo[3].encode('utf-8')
+            dbinfo_telno = dbinfo[4].encode('utf-8')
+            dbinfo_sleep = dbinfo[5]
+            dbinfo_callid = dbinfo[6]
+            dbinfo_attempt = dbinfo[7]
+            dbinfo_latesttime = dbinfo[8].encode('utf-8')
+            dbinfo_lateststatus = dbinfo[9]
+            result = tw.call(dbinfo_eventid, dbinfo_numorder, dbinfo_ghid, dbinfo_name, dbinfo_telno, evinfo_testflg, evinfo_hostname, evinfo_message, evinfo_headid, evinfo_footid) 
+            #result = tw.call('ichiro', 'suzuki', '0000', '1', 'hoge.example.com', 'message is hoge', '1', '1')
