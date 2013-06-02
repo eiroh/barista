ec4bc335780775d897dfe95dd80e0cdb3a27d9b4
HiDE <vinasaigon@gmail.com>
2013-06-03 00:16:41 +0900

    initial commit

---
+++ barista.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os.path
+import logging
+import string
+import time
+from twiliomanage import twiliomanage
+import define
+import ConfigParser
+from pyres import ResQ
+from eventq import eventQ
+
+conf = ConfigParser.SafeConfigParser()
+conf.read('settings.ini')
+options_resqserver = conf.get('ResQ', 'server')
+options_resqport   = conf.get('ResQ', 'port')
+
+class barista():
+
+    def firstRegist(self, testflg, hostname, operator, calltype, frequency, language, message, headid, footid, addressee):
+
+        tw = twiliomanage()
+        eventid = tw.geteventid()
+        status = define.EVENT_STATUS['WAITING']
+        lastnum = 0
+        result = tw.eventregister(eventid, status, testflg, hostname, operator, calltype, frequency, language, message, headid, footid, lastnum)
+
+        for record in addressee:
+            param = record.split(':')
+            numorder = param[0]
+            ghid = param[1]
+            telno = param[2]
+            name = param[3]
+            sleep = param[4]
+            callid = 0
+            attempt = 0
+            latesttime = 0
+            lateststatus = define.CALL_STATUS['WAITING']
+            result = tw.callregister(eventid, numorder, ghid, name, telno, sleep, callid, attempt, latesttime, lateststatus)
+
+        r = ResQ(server="%s:%s" % (options_resqserver, options_resqport))
+        r.enqueue(eventQ, eventid)
+
+        return eventid
